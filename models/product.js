28f26be71d81602b467962f1f3c60e92855cb9bb
MUNNY KUMAR <36681274+storyofcoder@users.noreply.github.com>
2019-06-02 01:51:28 +0530

    initial

---
+++ models/product.js
@@ -0,0 +1,15 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var schema = new Schema({
+
+    imagePath:{type:String, required:true},
+    title:{type:String, required: true},
+    description:{type:String, required:true},
+    vitamins:{type:String, required:true},
+    price:{type:Number, required:true},
+    weight:{type:String, required:true}
+
+});
+
+module.exports = mongoose.model('Product',schema);
